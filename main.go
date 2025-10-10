cf99ac732462d30ccfdbe0ccfcb3cd184492546a
alejandroalbarran98102@gmail.com <alejandro@Battlestation.>
2025-10-10 19:31:25 -0400

    Added cleanInput method and testing

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
+func cleanInput(text string) []string {
+	words := strings.Fields(strings.ToLower(text))
+	return words
+}
+
 func main(){
-	fmt.Println("Hello world")
+	fmt.Println("Hello, World!")
 }
---
+++ repl_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+    cases := []struct {
+	input    string
+	expected []string
+}{
+	{
+		input:    "hello  world",
+		expected: []string{"hello", "world"},
+	},
+	// add more cases here
+}
+for _, c := range cases {
+	actual := cleanInput(c.input)
+	if len(actual) != len(c.expected) {
+		t.Errorf("Not the same length!")
+		continue
+	}
+	
+	for i := range actual {
+		word := actual[i]
+		expectedWord := c.expected[i]
+		if word != expectedWord {
+			t.Errorf("Words Dont match!")
+		}
+	}
+}
+
+}
