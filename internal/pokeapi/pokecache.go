a267a1b73a5350216ae7cd0fc29ecba09ed3b1f6
alejandroalbarran98102@gmail.com <alejandro@Battlestation.>
2025-10-12 22:48:17 -0400

    Added partial cache functionality

---
+++ internal/pokeapi/pokecache.go
@@ -0,0 +1,54 @@
+package pokeapi
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	entry map[string]cacheEntry
+	mutex sync.Mutex
+	interval time.Duration
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val []byte
+}
+
+
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
+		entry: make(map[string]cacheEntry),
+		interval: interval,
+	}
+	go c.reapLoop()
+	return c
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	// lock so no one can touch while we add
+	c.mutex.Lock()
+	// unlock after we leave the function
+	defer c.mutex.Unlock()
+	//add new data to the cache
+	c.entry[key] = cacheEntry{
+		createdAt: time.Now(),
+		val: val,
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	// lock before getting
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	e, ok := c.entry[key]
+	if !ok {
+		return nil, false
+	}
+	return e.val, true
+}
+
+func reapLoop() {
+
+}
